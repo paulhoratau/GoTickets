23929b37d9f5cc2a028b0aa079178a39d714ca55
Paul Horatau <paulh@192.168.1.12>
2024-04-18 21:46:33 +0300

    added register function

--- GoTicketsApp/views.py
+++ GoTicketsApp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'GoTickets/index.html')
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect ('login')
+    context = {'form': form}
+    return render(request, 'GoTickets/register.html', context)
